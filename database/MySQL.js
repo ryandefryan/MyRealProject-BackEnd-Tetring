72502bca843aeda7a747be3f1cb774ba9887c4e9
ryandefryan <ryan.fandy@gmail.com>
2020-10-09 11:06:45 +0700

    Added SQL Connection for Deploying on Database > MySQL.js

--- database/MySQL.js
+++ database/MySQL.js
@@ -1,19 +1,20 @@
 const mysql = require('mysql')
 
 // ############### BEFORE DEPLOY ###############
-const db = mysql.createConnection({
-    user : 'root',
-    password : '20121995',
-    database : 'authentic_system',
-    port : '3306'
-})
-
-// ############### AFTER DEPLOY ###############
 // const db = mysql.createConnection({
-//     user : ' sql12369461',
-//     password : '346V54Wkv2',
-//     database : 'sql12369461',
+//     user : 'root',
+//     password : '20121995',
+//     database : 'authentic_system',
 //     port : '3306'
 // })
+
+// ############### AFTER DEPLOY ###############
+const db = mysql.createConnection({
+    host : 'sql12.freemysqlhosting.net',
+    user : 'sql12369461',
+    password : '346V54Wkv2',
+    database : 'sql12369461',
+    port : '3306'
+})
 
 module.exports = db
